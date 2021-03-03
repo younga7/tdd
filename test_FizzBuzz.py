7ae15a96e6126118e814d0994e04f4bda4c09a2b
Alex Young <younga6@oregonstate.edu>
2021-03-03 00:24:20 -0800

    Update first test (forgot __name__), this checks if FizzBuzz program correctly implements its logic

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -1,7 +1,7 @@
 # CS362 test_FizzBuzz.py
 # Alex Young
 # 3/2/2021
-# Run this file using python3 test_fizzbuzz.py or just pytest
+# Run this file using python3 test_FizzBuzz.py or just pytest
 # This program tests if the FizzBuzz program works correctly
 
 import unittest
@@ -18,3 +18,7 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(FizzBuzz.run(5), "Buzz")
         self.assertEqual(FizzBuzz.run(15), "FizzBuzz")
         self.assertEqual(FizzBuzz.run("10"), "Type Error")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
